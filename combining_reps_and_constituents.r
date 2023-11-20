bbce0376f8815e5c3d4df01a8731d21a2f0b6c5c
jennet-zamanova <zamanova@mit.edu>
2023-11-19 21:32:06 -0500

    cleaned up the constituent votes and added representatives
    The indexed votes is the csv we want to use from now on

---
+++ combining_reps_and_constituents.r
@@ -0,0 +1,39 @@
+library("dplyr")
+
+data_reps <- read.csv("final_votes/ca_representative_votes.csv")
+data_consts <- read.csv("final_votes/ca_constituent_votes.csv")
+
+convert_values <- function(x) {
+  ifelse(x == "yes", 1, ifelse(x == "no", 0, ifelse(x %in% c("Absent", "NV"), -1, NA)))
+}
+# not really binary lol
+binary_reps <- data_reps%>%
+  mutate_at(vars(-Districts), convert_values)
+
+# a bit messy merge to get idx - Thanks ChatGPT)
+# Merge dataframes on the 'ID' column
+merged_df <- merge(binary_reps, data_consts, by = 'Districts', suffixes = c('_rep', '_const'))
+
+# Initialize an empty 'Match' column
+merged_df$Match <- 1
+
+# Iterate over columns and update 'Match' column based on equality
+for (col in colnames(data_consts)[-1]) {
+  match_col <- paste0('Match_', col)
+  merged_df[[match_col]] <- ifelse(merged_df[[paste0(col, '_rep')]] == merged_df[[paste0(col, '_const')]], 
+                                   1, 
+                                   ifelse(merged_df[[paste0(col, '_rep')]] == -1, NA, 0))
+  merged_df$Match <- merged_df$Match * merged_df[[match_col]]
+}
+
+# Drop the redundant columns if needed
+merged_df <- merged_df[c("Districts", grep("Match", colnames(merged_df), value = TRUE))] 
+
+# remove the Match
+merged_df <- merged_df[, -2]
+match_columns <- grep("^Match_", colnames(merged_df), value = TRUE)
+new_column_names <- sub("^Match_", "", match_columns)
+# Rename the columns
+colnames(merged_df) <- c("Districts", new_column_names) 
+
+write.csv(new_data_s, "indexed_votes.csv", row.names = FALSE)
---
+++ getting_results_for_constituents.r
@@ -0,0 +1,56 @@
+# This code aims to get the constituent 
+# voting data based on district for each 
+# referendum and create a new dataframe 
+# with results of these votes
+
+# Get the results per district from the proposition
+get_results <- function (file_path){
+  data <- read.csv(file_path)
+  data$Percent <- ifelse(grepl("Percent", data$X.1), 1, 0)
+  data$Result <- ifelse(data$Percent == 1,
+                         ifelse(as.integer(gsub("%", "", data[, 3])) > as.integer(gsub("%", "", data[, 4])), 1, 0), NA)
+  prop <- data[!is.na(data$Result), ]
+  rownames(prop) <- 1:nrow(prop)
+  column <- paste0("Dsitrict ", rownames(prop))
+  prop[, 1] <- column
+  prop <- prop[, -c(2, 5)]
+  colnames(prop) <- c("District Name", "Yes", "No", "Result")
+  return(prop)
+}
+
+# Specify the folder path where your CSV files are located
+folder_path <- "raw_votes"
+
+# Get a list of CSV files in the folder
+csv_files <- list.files(folder_path, pattern = "\\.csv$", full.names = TRUE)
+
+dictionary_of_dfs <- list()
+# Iterate through each CSV file and apply the function
+for (file in csv_files) {
+  file_name <- as.character(tools::file_path_sans_ext(basename(file)))
+  ref_num <- paste0("R", substr(file_name, nchar(file_name) - 6, nchar(file_name)))
+  result <- get_results(file)
+  dictionary_of_dfs[[ref_num]] = result
+}
+
+# 2000 data for props 30 and 31
+ref2000 <- read.csv("raw_votes/exceptions/referendum ca - Ref2000.csv")
+ref2000 <- ref2000[, c(1, 2)]
+dummy_df <- data.frame(matrix(ncol = 0, nrow = 80))
+dummy_df$Result <- ref2000[, 1]
+dictionary_of_dfs[["R30_2000"]] = dummy_df
+dummy_df <- data.frame(matrix(ncol = 0, nrow = 80))
+dummy_df$Result <- ref2000[, 2]
+dictionary_of_dfs[["R31_2000"]] = dummy_df
+
+# Create an empty data frame
+new_data_s <- data.frame(matrix(ncol = 0, nrow = 80))
+column <- rownames(new_data_s)
+new_data_s[["Districts"]] <- column # just so same as reps df
+# Loop to populate the columns
+for (ref in names(dictionary_of_dfs)) {
+  res <- dictionary_of_dfs[[as.character(ref)]]
+  new_data_s[[as.character(ref)]] <- res$Result
+}
+
+write.csv(new_data_s, "final_votes/ca_constituent_votes.csv", row.names = FALSE)
